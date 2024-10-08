7193669b70a1130b37276e67f021ca4a2b8a23fb
조영상 <51320595+0ssang@users.noreply.github.com>
2024-10-08 17:58:47 +0900

    MongoDB 연동

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log("tlqkf");
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+// JSON 형태의 요청 body를 파싱하기 위해 express.json() 미들웨어 사용 
+
+app.use(express.urlencoded({ extended: false }));
+
+// 인코딩 된 request의 payload를 파싱해주는 미들웨어다.
+
+const port = 3000;
+const uri = "mongodb+srv://detective:ysh31228*@cluster0.4huxy.mongodb.net/coinner?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(uri).then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+});
