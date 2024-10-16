feec8dfa5e625117b3613689a0f7e1ca5d721516
조영상 <51320595+0ssang@users.noreply.github.com>
2024-10-16 22:11:17 +0900

    feat: Post schema - 커스텀 메서드 구현 조회수 증가 기능, 유저별  좋아요 추가/제거 메서드

--- src/models/post.js
+++ src/models/post.js
@@ -25,6 +25,10 @@ const PostSchema = new Schema({
         type: Number, 
         default: 0 
     },
+    likes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
     updatedAt: {
         type: Date,
         default: Date.now
@@ -40,5 +44,34 @@ PostSchema.pre('save', async function(next) {
     this.updatedAt = Date.now();
     next();
 });
+
+// 조회수 증가 메서드
+PostSchema.methods.increaseViews = async function() {
+    this.views += 1;
+    await this.save();
+    return this;
+};
+
+// 좋아요 추가 메서드
+PostSchema.methods.addLike = async function(userId) {
+    if(!this.likes.includes(userId)) {
+        this.likes.push(userId);
+        await this.save();
+        return this;
+    }
+    return this;
+}
+
+// 좋아요 취소 메서드
+PostSchema.methods.removeLike = async function(userId) {
+    const index = this.likes.indexOf(userId);
+    if(index > -1) {
+        // 좋아요 리스트에서 제거
+        this.likes.splice(index, 1);
+        await this.save();
+        return this;
+    }
+    return this;
+}
 
 module.exports = mongoose.model('Post', PostSchema);
