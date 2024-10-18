2b2e65216ff95eb100287059912f50c5583f258a
조영상 <51320595+0ssang@users.noreply.github.com>
2024-10-18 20:31:04 +0900

    refactor: 게시글 작성시 트랜잭션 적용 게시글 작성 & 유저 포스팅 정보 업데이트 기능에 트랜잭션 적용 Mongoose session 이용하여 트랜잭션 구현

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,12 +1,16 @@
+const mongoose = require('mongoose');
 const Post = require('../models/post');
 const User = require('../models/user');
 
-// 게시글 작성 로직 (메니저 계정 테스트용)
+// 게시글 작성 로직 (트랜잭션 적용)
 exports.createPost = async (title, content, username) => {
+    // MongoDB 세션 시작
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
     try {
         // 사용자 찾기
-        const user = await User.findOne({ username });
-
+        const user = await User.findOne({ username }).session(session);
         if (!user) {
             throw new Error('User not found');
         }
@@ -19,16 +23,25 @@ exports.createPost = async (title, content, username) => {
         });
 
         // 게시글 저장
-        const savePost = await newPost.save();
-        if(savePost) {
-            // 사용자의 posts 배열에 게시글 id 업데이트
-            user.posts.push(newPost._id);
-            return await user.save();
-        }
+        await newPost.save({ session }); // 트랜잭션 내에서 저장
+
+        // 사용자의 posts 필드에 게시글 id 추가
+        user.posts.push(newPost._id);
+
+        // 사용자 업데이트 저장
+        await user.save({ session }); // 트랜잭션 내에서 저장
+        
+        // 트랜잭션 커밋 (성공적으로 완료)
+        await session.commitTransaction();
+
+        return newPost;
     } catch (error) {
+        // 트랜잭션 롤백 (오류 발생 시)
+        await session.abortTransaction();
         throw new Error(error.message);
     } finally {
-
+        // 세션 종료
+        session.endSession();
     }
 };
 
